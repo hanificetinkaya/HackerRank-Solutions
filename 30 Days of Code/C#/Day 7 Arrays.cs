c1aeb09fe44e80fbed63f292afc787c9d4065da2
Hanifi ÇETİNKAYA <33221074+hanificetinkaya@users.noreply.github.com>
2024-04-09 17:36:10 +0100

    Create Day 7 Arrays.cs

---
+++ 30 Days of Code/C#/Day 7 Arrays.cs
@@ -0,0 +1,48 @@
+﻿╔══════════════════════════════════════════════════════════════════════════════════════════╗
+║                                 Information                                              ║
+╟──────────────────────────────────────────────────────────────────────────────────────────╢
+║   Direct Link: https://www.hackerrank.com/challenges/30-arrays/problem                   ║
+║   Difficulty: Easy                                                                       ║
+║   Language: C#                                                                           ║
+╠══════════════════════════════════════════════════════════════════════════════════════════╣
+║   Solutioin                                                                              ║
+╚══════════════════════════════════════════════════════════════════════════════════════════╝
+
+
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+        int count=arr.Count();
+        for(int i=0;i<count/2;i++)
+        {
+            int t=arr[i];
+            arr[i]=arr[count-1-i];
+            arr[count-1-i]=t;
+        }
+        
+        for(int i=0;i<count;i++)
+        {
+            Console.Write(arr[i]+" ");
+        }
+    }
+}
