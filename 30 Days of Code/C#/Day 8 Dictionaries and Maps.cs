57fee3e295a5dee994ef6add9c3ab115c3f4e856
Hanifi ÇETİNKAYA <33221074+hanificetinkaya@users.noreply.github.com>
2024-04-10 14:15:55 +0100

    Create Day 8 Dictionaries and Maps.cs

---
+++ 30 Days of Code/C#/Day 8 Dictionaries and Maps.cs
@@ -0,0 +1,34 @@
+﻿╔══════════════════════════════════════════════════════════════════════════════════════════╗
+║                                 Information                                              ║
+╟──────────────────────────────────────────────────────────────────────────────────────────╢
+║   Direct Link: https://www.hackerrank.com/challenges/30-dictionaries-and-maps/problem    ║
+║   Difficulty: Easy                                                                       ║
+║   Language: C#                                                                           ║
+╠══════════════════════════════════════════════════════════════════════════════════════════╣
+║   Solutioin                                                                              ║
+╚══════════════════════════════════════════════════════════════════════════════════════════╝
+
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+class Solution {
+    static void Main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution */
+        int n = Convert.ToInt32(Console.ReadLine());
+        Dictionary<string, int> dic=new Dictionary<string, int>();
+        for(int i=0;i<n;i++)
+        {
+            string data=Console.ReadLine();
+            dic.Add(data.Split(' ')[0],Convert.ToInt32(data.Split(' ')[1]));
+        }
+        
+        for(int i=0;i<n;i++)
+        {
+            string name=Console.ReadLine();
+            if(dic.ContainsKey(name))
+                Console.WriteLine(name+"="+dic[name]);            
+            else Console.WriteLine("Not found");
+        }
+    }
+}
