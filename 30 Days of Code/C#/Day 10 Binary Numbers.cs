217d5fdff0e79cca7438dd23a5b66d3b7ed48cae
Hanifi ÇETİNKAYA <hancet@msn.com>
2024-04-12 12:17:07 +0100

    Create Day 10 Binary Numbers.cs

---
+++ 30 Days of Code/C#/Day 10 Binary Numbers.cs
@@ -0,0 +1,59 @@
+﻿╔══════════════════════════════════════════════════════════════════════════════════════════╗
+║                                 Information                                              ║
+╟──────────────────────────────────────────────────────────────────────────────────────────╢
+║   Direct Link: https://www.hackerrank.com/challenges/30-binary-numbers/problem           ║
+║   Difficulty: Easy                                                                       ║
+║   Language: C#                                                                           ║
+╠══════════════════════════════════════════════════════════════════════════════════════════╣
+║   Solutioin                                                                              ║
+╚══════════════════════════════════════════════════════════════════════════════════════════╝
+
+
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+        string bit=convertToBinary(n);
+        int say=0;
+        int enCok=0;
+        for(int i=0;i<bit.Length;i++)
+        {
+            if(bit[i]=='1') say++;
+            else
+            {
+                if(say>enCok) enCok=say;
+                say=0;
+            }
+        }
+        if(say>enCok) enCok=say;
+        Console.WriteLine(enCok);
+    }
+    
+    public static string convertToBinary(int n)
+    {
+        string bit="";
+        while(n!=0)
+        {
+            bit+=n%2;
+            n=n/2;
+        }
+        return bit;
+    }
+}
