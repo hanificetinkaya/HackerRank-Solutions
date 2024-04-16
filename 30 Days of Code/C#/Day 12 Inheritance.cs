f6699398a3e2eb7effef9b5bf53a90fa89e0394c
Hanifi ÇETİNKAYA <hancet@msn.com>
2024-04-16 14:54:24 +0100

    Create Day 12 Inheritance.cs

---
+++ 30 Days of Code/C#/Day 12 Inheritance.cs
@@ -0,0 +1,91 @@
+﻿╔══════════════════════════════════════════════════════════════════════════════════════════╗
+║                                 Information                                              ║
+╟──────────────────────────────────────────────────────────────────────────────────────────╢
+║   Direct Link: https://www.hackerrank.com/challenges/30-inheritance/problem              ║
+║   Difficulty: Easy                                                                       ║
+║   Language: C#                                                                           ║
+╠══════════════════════════════════════════════════════════════════════════════════════════╣
+║   Solutioin                                                                              ║
+╚══════════════════════════════════════════════════════════════════════════════════════════╝
+
+
+using System;
+using System.Linq;
+
+class Person{
+	protected string firstName;
+	protected string lastName;
+	protected int id;
+	
+	public Person(){}
+	public Person(string firstName, string lastName, int identification){
+			this.firstName = firstName;
+			this.lastName = lastName;
+			this.id = identification;
+	}
+	public void printPerson(){
+		Console.WriteLine("Name: " + lastName + ", " + firstName + "\nID: " + id); 
+	}
+}
+
+class Student : Person{
+    private int[] testScores;  
+  
+    /*	
+    *   Class Constructor
+    *   
+    *   Parameters: 
+    *   firstName - A string denoting the Person's first name.
+    *   lastName - A string denoting the Person's last name.
+    *   id - An integer denoting the Person's ID number.
+    *   scores - An array of integers denoting the Person's test scores.
+    */
+    // Write your constructor here
+    public Student(string firstName, string lastName, int id, int[] testScores): base(firstName, lastName, id)
+    {
+        this.testScores=testScores;
+    }
+    /*	
+    *   Method Name: Calculate
+    *   Return: A character denoting the grade.
+    */
+    // Write your method here
+    
+    public char Calculate()
+    {
+        int sum=0, avg=0;
+        
+        for(int i = 0; i < testScores.Length; i++)
+        {
+            sum += testScores[i];
+        }
+        if(testScores.Length != 0)
+            avg = sum/testScores.Length;
+            
+        if(avg>=90) return 'O';
+        else if(avg>=80) return 'E';
+        else if (avg>=70) return 'A';
+        else if (avg>=55) return 'P';
+        else if (avg>=40) return 'D';
+        else return 'T';
+    }
+}
+
+class Solution {
+	static void Main() {
+		string[] inputs = Console.ReadLine().Split();
+		string firstName = inputs[0];
+	  	string lastName = inputs[1];
+		int id = Convert.ToInt32(inputs[2]);
+		int numScores = Convert.ToInt32(Console.ReadLine());
+		inputs = Console.ReadLine().Split();
+	  	int[] scores = new int[numScores];
+		for(int i = 0; i < numScores; i++){
+			scores[i]= Convert.ToInt32(inputs[i]);
+		} 
+	  	
+		Student s = new Student(firstName, lastName, id, scores);
+		s.printPerson();
+		Console.WriteLine("Grade: " + s.Calculate() + "\n");
+	}
+}
