993f4a32f19903b864d8ca408643bfa211ea66a7
Hanifi ÇETİNKAYA <hancet@msn.com>
2024-04-20 08:15:28 +0100

    Create Day 16 Exceptions - String to Integer.cs

---
+++ 30 Days of Code/C#/Day 16 Exceptions - String to Integer.cs
@@ -0,0 +1,41 @@
+﻿╔══════════════════════════════════════════════════════════════════════════════════════════════╗
+║                                 Information                                                  ║
+╟──────────────────────────────────────────────────────────────────────────────────────────────╢
+║   Direct Link: https://www.hackerrank.com/challenges/30-exceptions-string-to-integer/problem ║
+║   Difficulty: Easy                                                                           ║
+║   Language: C#                                                                               ║
+╠══════════════════════════════════════════════════════════════════════════════════════════════╣
+║   Solutioin                                                                                  ║
+╚══════════════════════════════════════════════════════════════════════════════════════════════╝
+
+
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        string S = Console.ReadLine();
+        try
+        {
+            int a=Convert.ToInt32(S);
+            Console.WriteLine(a);
+        }
+        catch(Exception e)
+        {
+            Console.WriteLine("Bad String");
+        }
+    }
+}
