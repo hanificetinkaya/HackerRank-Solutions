67ba2b05573e55bd4a6e8f340d0115d4a981897f
Hanifi ÇETİNKAYA <hancet@msn.com>
2024-04-30 16:28:46 +0100

    Create Day 24 More Linked Lists.cs

---
+++ 30 Days of Code/C#/Day 24 More Linked Lists.cs
@@ -0,0 +1,80 @@
+﻿╔══════════════════════════════════════════════════════════════════════════════════════════╗
+║                                 Information                                              ║
+╟──────────────────────────────────────────────────────────────────────────────────────────╢
+║   Direct Link: https://www.hackerrank.com/challenges/30-linked-list-deletion/problem     ║
+║   Difficulty: Easy                                                                       ║
+║   Language: C#                                                                           ║
+╠══════════════════════════════════════════════════════════════════════════════════════════╣
+║   Solutioin                                                                              ║
+╚══════════════════════════════════════════════════════════════════════════════════════════╝
+
+
+using System;
+using System.Collections.Generic;
+class Node
+{
+	public int data;
+	public Node next;
+    public Node(int d){
+        data=d;
+        next=null;
+    }
+		
+}
+class Solution {
+
+  public static Node removeDuplicates(Node head){
+    //Write your code here
+    if (head==null) return head;
+    
+    Node curr=head;
+    
+    while(curr.next!=null)
+    {
+        if(curr.data==curr.next.data) curr.next=curr.next.next;
+        else curr=curr.next;
+    }
+    
+    return head;
+  }
+
+	public static  Node insert(Node head,int data)
+	{
+        Node p=new Node(data);
+		
+		
+		if(head==null)
+			head=p;
+		else if(head.next==null)
+			head.next=p;
+		else
+		{
+			Node start=head;
+			while(start.next!=null)
+				start=start.next;
+			start.next=p;
+			
+		}
+		return head;
+    }
+	public static void display(Node head)
+	{
+		Node start=head;
+		while(start!=null)
+		{
+			Console.Write(start.data+" ");
+			start=start.next;
+		}
+	}
+    static void Main(String[] args) {
+	
+		Node head=null;
+        int T=Int32.Parse(Console.ReadLine());
+        while(T-->0){
+            int data=Int32.Parse(Console.ReadLine());
+            head=insert(head,data);
+        }
+      	head=removeDuplicates(head);
+		display(head);
+	}
+}
