d43c1ced5e7a49bc53bc5c3ca41baf214fc29309
Hanifi ÇETİNKAYA <hancet@msn.com>
2024-05-01 12:54:25 +0100

    Create Day 25 Running Time and Complexity.cs

---
+++ 30 Days of Code/C#/Day 25 Running Time and Complexity.cs
@@ -0,0 +1,41 @@
+﻿╔═════════════════════════════════════════════════════════════════════════════════════════════╗
+║                                 Information                                                 ║
+╟─────────────────────────────────────────────────────────────────────────────────────────────╢
+║   Direct Link: https://www.hackerrank.com/challenges/30-running-time-and-complexity/problem ║
+║   Difficulty: Medium                                                                        ║
+║   Language: C#                                                                              ║
+╠═════════════════════════════════════════════════════════════════════════════════════════════╣
+║   Solutioin                                                                                 ║
+╚═════════════════════════════════════════════════════════════════════════════════════════════╝
+
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+class Solution {
+    static void Main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution */
+        int t=Convert.ToInt32(Console.ReadLine());
+        for(int i=0;i<t;i++)
+        {
+            int n=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(CheckPrime(n));
+        }
+    }
+    
+    public static string CheckPrime(int number)
+    {
+        if (number==2 || number==3 || number==5) return "Prime";
+        bool check=false;
+        for(int i=2;i<number/2;i++)
+        {
+            if(number%i==0) return "Not prime";
+            else
+            {
+                check=true;
+            }
+        }
+        if (check) return "Prime";
+        else return "Not prime";
+    }
+}
