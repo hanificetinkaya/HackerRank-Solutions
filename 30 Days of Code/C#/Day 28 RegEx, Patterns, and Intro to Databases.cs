606ed520482da447202501a924d15f925faccf88
Hanifi ÇETİNKAYA <hancet@msn.com>
2024-05-03 15:54:35 +0100

    Create Day 28 RegEx, Patterns, and Intro to Databases.cs

---
+++ 30 Days of Code/C#/Day 28 RegEx, Patterns, and Intro to Databases.cs
@@ -0,0 +1,53 @@
+﻿╔══════════════════════════════════════════════════════════════════════════════════════════╗
+║                                 Information                                              ║
+╟──────────────────────────────────────────────────────────────────────────────────────────╢
+║   Direct Link: https://www.hackerrank.com/challenges/30-regex-patterns/problem           ║
+║   Difficulty: Medium                                                                     ║
+║   Language: C#                                                                           ║
+╠══════════════════════════════════════════════════════════════════════════════════════════╣
+║   Solutioin                                                                              ║
+╚══════════════════════════════════════════════════════════════════════════════════════════╝
+
+
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        int N = Convert.ToInt32(Console.ReadLine().Trim());
+        
+        Regex regex = new Regex("[a-z]+@gmail\\.com$");
+        var names = new List<string>();
+
+        for (int NItr = 0; NItr < N; NItr++)
+        {
+            string[] firstMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
+
+            string firstName = firstMultipleInput[0];
+
+            string emailID = firstMultipleInput[1];
+            
+            if(regex.IsMatch(emailID)) names.Add(firstName);
+        }
+        
+        names.Sort();
+        
+        foreach(var n in names)
+            Console.WriteLine(n);
+    }
+}
